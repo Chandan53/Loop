4b208da97e20d2e46bad85847c806894a7908221
chandan <chandankumar936672@gmail.com>
2025-07-21 15:36:17 +0530

    do while loop

---
+++ loop7.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class loop7 {
+    public static void main(String args[]){
+        // do while loop
+        int count=1;
+        do{
+            System.out.println(count);
+            count++;
+        } while(count<=10);
+    }
+    
+}
