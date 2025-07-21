241b3e69fafbbb79937ac393b1f26f524fcf7c20
chandan <chandankumar936672@gmail.com>
2025-07-21 15:27:17 +0530

    reverse the given number bu user

---
+++ loop6.java
@@ -0,0 +1,19 @@
+package Loop;
+import java.util.*;
+
+public class loop6 {
+    public static void main(String args[]){
+        // reverse the given number
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        while(num>0){
+            int lastdigit=num%10;
+            System.out.print(lastdigit);
+            num=num/10;
+        }
+        System.out.println();
+
+        
+    }
+    
+}
