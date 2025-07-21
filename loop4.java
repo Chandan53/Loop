b22e4b62c428add6abb7ae36798c991f41fd50c1
chandan <chandankumar936672@gmail.com>
2025-07-21 10:43:53 +0530

    for loop

---
+++ loop4.java
@@ -0,0 +1,14 @@
+package Loop;
+
+public class loop4 {
+    public static void main(String args[]){
+        // for loop
+        // syntax
+        // for(initialisation;condition;updation){
+        //}
+        for(int i=1;i<=5;i++){
+            System.out.println("chandan");
+        }
+    }
+    
+}
