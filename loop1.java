3764da760e6453d90e6059f388e2a3201632f804
chandan <chandankumar936672@gmail.com>
2025-07-21 09:56:51 +0530

    while loop

---
+++ loop1.java
@@ -0,0 +1,33 @@
+package Loop;
+
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
+public class loop1 {
+    public static void main(String args[]){
+        // in java we have 3 loop
+        
+        // while loop
+        // for loop
+        // do while loop
+        
+
+        // while loop
+        /* syntax
+        // initialization
+         * while(condition){
+         * do something
+         * }
+         *  up_dation
+         */
+
+         // print hello world 100 times using while loop
+        int count =1; // --> initialization
+         while(count<=10){ // --> condition
+            System.out.println("chandan");
+            count++; // --> updation
+         }
+         System.out.println("10 times printed");
+
+    }
+    
+}
